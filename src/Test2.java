99f6b5d5a5c5377bba0e8535846511493cf1cfce
Aleksei Fefelov <avvfefelov@yahoo.com>
2024-12-10 15:02:29 +0400

    Git test3

---
+++ src/Test2.java
@@ -0,0 +1,9 @@
+// Второй файл, чтобы проверить
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("Тест2");
+        System.out.println("Тест22");
+    }
+}
--- src/TestGH.java
+++ src/TestGH.java
@@ -5,7 +5,6 @@ public class TestGH {
     public static void main(String[] args) {
 
         System.out.println("Учусь работать с GitHub");
-
+        System.out.println("Тест1");
     }
-
 }
