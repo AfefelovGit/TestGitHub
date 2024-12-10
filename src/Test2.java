c8b95479f04b8531ba409122933c76959ce80f42
Aleksei Fefelov <avvfefelov@yahoo.com>
2024-12-10 15:17:46 +0400

    Git test4

--- src/Test2.java
+++ src/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 
     public static void main(String[] args) {
         System.out.println("Тест2");
+        //
         System.out.println("Тест22");
     }
 }
--- src/TestGH.java
+++ src/TestGH.java
@@ -3,7 +3,7 @@
 public class TestGH {
 
     public static void main(String[] args) {
-
+        // комент
         System.out.println("Учусь работать с GitHub");
         System.out.println("Тест1");
     }
